f8324397a7b74666158e7b9a75e9a62e8f45f86c
Hugo Cântara <hugo.cantara@gmail.com>
2024-08-08 17:59:23 +0100

    Configure Swagger

--- DigitalPoetry.LibraryManager.Service.API/Program.cs
+++ DigitalPoetry.LibraryManager.Service.API/Program.cs
@@ -1,11 +1,14 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o => AddSwaggerDocumentation(o));
 
 var app = builder.Build();
 
@@ -13,7 +16,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BookLoanManagementService.API v1"));
 }
 
 app.UseHttpsRedirection();
@@ -23,3 +26,10 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+void AddSwaggerDocumentation(SwaggerGenOptions o)
+{
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    o.SwaggerDoc("v1", new OpenApiInfo { Title = "BookLoanManagementService.API", Version = "v1" });
+    o.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+}
